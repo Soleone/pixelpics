cf47305ea5c112d29340a52e271900d51b03e474
Dennis Theisen <dennis@shopify.com>
2018-07-22 01:02:14 -0400

    Enter transition and filled transition

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,10 +1,12 @@
 <template>
-  <td class="cell"
-       :class="{filled: cell.filled, selected: cell.selected, marked: cell.marked}"
-       @mousedown="primaryAction"
-       @contextmenu.prevent="secondaryAction"
-       @keyup.enter="primaryAction">
-  </td>
+  <transition :name="enterTransition" appear>
+    <td class="cell animated"
+         :class="{filled: cell.filled, selected: cell.selected, marked: cell.marked, bounceIn: cell.filled}"
+         @mousedown="primaryAction"
+         @contextmenu.prevent="secondaryAction"
+         @keyup.enter="primaryAction">
+    </td>
+  </transition>
 </template>
 
 
@@ -12,6 +14,11 @@
 export default {
   name: 'Cell',
   props: ['cell'],
+  data() {
+    return {
+      enterTransition: null
+    }
+  },
   methods: {
     primaryAction() {
       if (this.$store.state.editMode) {
@@ -33,12 +40,20 @@ export default {
     },
     toggleMark() {
       this.cell.marked = !this.cell.marked
+    },
+    randomBool() {
+      return (Math.random() < 0.5);
     }
   },
   computed: {
     displayFilled() {
       return (this.cell.filled && this.editMode);
     }
+  },
+  created() {
+    const verticalModifier = this.randomBool() ? '-vertical' : '';
+    const reverseModifier = this.randomBool() ? '-reverse' : '';
+    this.enterTransition = "slide-fade" + verticalModifier + reverseModifier;
   }
 }
 </script>
@@ -53,6 +68,7 @@ export default {
     border-left: 1px solid transparent;
     background-color: #e8e8e8;
     cursor: pointer;
+    -vendor-animation-duration: 0.1s;
   }
 
   .filled {
@@ -69,5 +85,32 @@ export default {
 
   .selected {
     background-color: #666;
+  }
+
+  .slide-fade-enter-active,
+  .slide-fade-reverse-enter-active,
+  .slide-fade-vertical-enter-active,
+  .slide-fade-vertical-reverse-enter-active {
+    transition: all .5s ease-out;
+  }
+
+  .slide-fade-enter {
+    transform: translateX(48px);
+    opacity: 0;
+  }
+
+  .slide-fade-reverse-enter {
+    transform: translateX(-48px);
+    opacity: 0;
+  }
+
+  .slide-fade-vertical-enter {
+    transform: translateY(48px);
+    opacity: 0;
+  }
+
+  .slide-fade-vertical-reverse-enter {
+    transform: translateY(-48px);
+    opacity: 0;
   }
 </style>
